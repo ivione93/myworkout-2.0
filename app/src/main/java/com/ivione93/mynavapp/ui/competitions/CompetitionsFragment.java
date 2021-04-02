e66df6cd64b63558deed9eaac1e162cc1b28e6be
Iván García <ivan.garcia.ext@diagroup.com>
2021-04-02 19:11:19 +0200

    rvCompetitions

--- app/src/main/java/com/ivione93/mynavapp/ui/competitions/CompetitionsFragment.java
+++ app/src/main/java/com/ivione93/mynavapp/ui/competitions/CompetitionsFragment.java
@@ -10,14 +10,29 @@ import android.widget.ImageButton;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
 
 import com.ivione93.mynavapp.MainActivity;
 import com.ivione93.mynavapp.R;
+import com.ivione93.mynavapp.db.AppDatabase;
+import com.ivione93.mynavapp.db.Competition;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CompetitionsFragment extends Fragment {
 
     private CompetitionsViewModel competitionsViewModel;
+
+    public static List<Competition> listCompetitions = new ArrayList<>();
+
+    RecyclerView rvCompetitions;
+    AdapterCompetition adapterCompetition = new AdapterCompetition(listCompetitions);
     ImageButton btnNewCompetition;
+
+    String license;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,12 +41,24 @@ public class CompetitionsFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_competitions, container, false);
         competitionsViewModel.getText().observe(getViewLifecycleOwner(), s -> {});
 
+        AppDatabase db = Room.databaseBuilder(root.getContext(),
+                AppDatabase.class, "database-name").fallbackToDestructiveMigration().allowMainThreadQueries().build();
+
+        rvCompetitions = root.findViewById(R.id.rvCompetitions);
+        rvCompetitions.setLayoutManager(new LinearLayoutManager(root.getContext()));
+
+        license = getActivity().getIntent().getStringExtra("license");
+        listCompetitions.clear();
+        listCompetitions.addAll(db.competitionDao().getCompetitionsByLicense(license));
+
         btnNewCompetition = root.findViewById(R.id.btnNewCompetition);
         btnNewCompetition.setOnClickListener(v -> {
             Intent newCompetition = new Intent(getActivity(), NewCompetition.class);
-            newCompetition.putExtra("license", getActivity().getIntent().getStringExtra("license"));
+            newCompetition.putExtra("license", license);
             container.getContext().startActivity(newCompetition);
         });
+
+        rvCompetitions.setAdapter(adapterCompetition);
         return root;
     }
 }
