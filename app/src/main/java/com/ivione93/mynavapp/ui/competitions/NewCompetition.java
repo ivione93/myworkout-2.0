ed4de64ca7c51d3aefba0d068cbea8fa5c2d9f56
Iván García <ivan.garcia.ext@diagroup.com>
2021-04-02 19:00:05 +0200

    New competition ready

--- app/src/main/java/com/ivione93/mynavapp/ui/competitions/NewCompetition.java
+++ app/src/main/java/com/ivione93/mynavapp/ui/competitions/NewCompetition.java
@@ -2,13 +2,28 @@ package com.ivione93.mynavapp.ui.competitions;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputLayout;
+import com.ivione93.mynavapp.MainActivity;
 import com.ivione93.mynavapp.R;
+import com.ivione93.mynavapp.Utils;
+import com.ivione93.mynavapp.db.AppDatabase;
+import com.ivione93.mynavapp.db.Competition;
 
 public class NewCompetition extends AppCompatActivity {
+
+    TextInputLayout placeText, competitionNameText, trackText, resultText;
+    EditText dateText;
+    Button btnSave;
 
     String license;
 
@@ -20,7 +35,37 @@ public class NewCompetition extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(R.string.new_competition);
 
+        AppDatabase db = Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class, "database-name").fallbackToDestructiveMigration().allowMainThreadQueries().build();
+
         license = getIntent().getStringExtra("license");
+
+        initReferences();
+
+        btnSave.setOnClickListener(view -> {
+            String place = placeText.getEditText().getText().toString();
+            String competitionName = competitionNameText.getEditText().getText().toString();
+            String track = trackText.getEditText().getText().toString();
+            String result = resultText.getEditText().getText().toString();
+            String date = dateText.getText().toString();
+
+            if (validateNewCompetition(place, competitionName, track, result, date)) {
+                Competition competition = new Competition(license,
+                        placeText.getEditText().getText().toString(),
+                        competitionNameText.getEditText().getText().toString(),
+                        Utils.toDate(dateText.getText().toString()),
+                        trackText.getEditText().getText().toString(),
+                        resultText.getEditText().getText().toString());
+                db.competitionDao().insert(competition);
+
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.putExtra("license", license);
+                startActivity(intent);
+            } else {
+                Toast toast = Toast.makeText(getApplicationContext(), "Faltan campos por completar", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
     }
 
     @Override
@@ -33,5 +78,35 @@ public class NewCompetition extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initReferences() {
+        placeText = findViewById(R.id.placeText);
+        competitionNameText = findViewById(R.id.competitionNameText);
+        trackText = findViewById(R.id.trackText);
+        resultText = findViewById(R.id.resultText);
+        placeText = findViewById(R.id.placeText);
+        dateText = findViewById(R.id.dateText);
+        btnSave = findViewById(R.id.saveButton);
+    }
+
+    private boolean validateNewCompetition(String place, String competitionName, String track, String result, String date) {
+        boolean isValid = true;
+        if(place.isEmpty() || place == null) {
+            isValid = false;
+        }
+        if(competitionName.isEmpty() || competitionName == null) {
+            isValid = false;
+        }
+        if(track.isEmpty() || track == null) {
+            isValid = false;
+        }
+        if(result.isEmpty() || result == null) {
+            isValid = false;
+        }
+        if(date.isEmpty() || date == null) {
+            isValid = false;
+        }
+        return isValid;
     }
 }
