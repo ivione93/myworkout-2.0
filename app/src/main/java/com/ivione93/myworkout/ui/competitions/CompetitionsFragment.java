4b9e4eee79e33e1c3fce5d8b6e9a2223c426da8c
Iván García <ivan.garcia.ext@diagroup.com>
2021-04-12 17:49:48 +0200

    Busqueda de competiciones

--- app/src/main/java/com/ivione93/myworkout/ui/competitions/CompetitionsFragment.java
+++ app/src/main/java/com/ivione93/myworkout/ui/competitions/CompetitionsFragment.java
@@ -1,13 +1,19 @@
 package com.ivione93.myworkout.ui.competitions;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -25,12 +31,12 @@ public class CompetitionsFragment extends Fragment {
 
     private CompetitionsViewModel competitionsViewModel;
 
-    public static List<Competition> listCompetitions = new ArrayList<>();
+    private List<Competition> listCompetitions = new ArrayList<>();
+    private AdapterCompetition adapterCompetition;
 
-    RecyclerView rvCompetitions;
-    AdapterCompetition adapterCompetition = new AdapterCompetition(listCompetitions);
     ImageButton btnNewCompetition;
 
+    AppDatabase db;
     String license;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -40,15 +46,16 @@ public class CompetitionsFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_competitions, container, false);
         competitionsViewModel.getText().observe(getViewLifecycleOwner(), s -> {});
 
-        AppDatabase db = Room.databaseBuilder(root.getContext(),
-                AppDatabase.class, "database-name").fallbackToDestructiveMigration().allowMainThreadQueries().build();
+        setHasOptionsMenu(true);
 
-        rvCompetitions = root.findViewById(R.id.rvCompetitions);
-        rvCompetitions.setLayoutManager(new LinearLayoutManager(root.getContext()));
+         db = Room.databaseBuilder(root.getContext(),
+                AppDatabase.class, "database-name").fallbackToDestructiveMigration().allowMainThreadQueries().build();
 
         license = getActivity().getIntent().getStringExtra("license");
         listCompetitions.clear();
         listCompetitions.addAll(db.competitionDao().getCompetitionsByLicense(license));
+
+        setUpRecyclerView(root);
 
         btnNewCompetition = root.findViewById(R.id.btnNewCompetition);
         btnNewCompetition.setOnClickListener(v -> {
@@ -57,7 +64,36 @@ public class CompetitionsFragment extends Fragment {
             container.getContext().startActivity(newCompetition);
         });
 
-        rvCompetitions.setAdapter(adapterCompetition);
         return root;
+    }
+
+    private void setUpRecyclerView(View root) {
+        RecyclerView rvCompetitions = root.findViewById(R.id.rvCompetitions);
+        rvCompetitions.setHasFixedSize(true);
+        adapterCompetition = new AdapterCompetition(listCompetitions);
+
+        rvCompetitions.setLayoutManager(new LinearLayoutManager(root.getContext()));
+        rvCompetitions.setAdapter(adapterCompetition);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.list_competitions_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.menu_search_competition);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapterCompetition.getFilter().filter(newText);
+                return false;
+            }
+        });
+        super.onCreateOptionsMenu(menu, inflater);
     }
 }
