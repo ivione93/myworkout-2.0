e35e8171b44aa1f595fad21a612ab1acb222db9a
Iván García <ivan.garcia.ext@diagroup.com>
2021-04-24 15:34:35 +0200

    Cambios

--- app/src/main/java/com/ivione93/myworkout/db/AppDatabase.java
+++ app/src/main/java/com/ivione93/myworkout/db/AppDatabase.java
@@ -8,7 +8,7 @@ import androidx.room.TypeConverters;
         Athlete.class,
         Competition.class,
         Training.class,
-        Series.class}, version = 1, exportSchema = false)
+        Series.class}, version = 2, exportSchema = false)
 @TypeConverters({Converters.class})
 public abstract class AppDatabase extends RoomDatabase {
     public abstract AthleteDao athleteDao();
