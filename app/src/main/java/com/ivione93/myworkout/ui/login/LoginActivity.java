fd47387e7580f92e4e79e5c6e1d63df6473527af
Iván García <ivan.garcia.ext@diagroup.com>
2021-04-25 11:38:24 +0200

    Control de inicio de sesion

--- app/src/main/java/com/ivione93/myworkout/ui/login/LoginActivity.java
+++ app/src/main/java/com/ivione93/myworkout/ui/login/LoginActivity.java
@@ -1,12 +1,19 @@
 package com.ivione93.myworkout.ui.login;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -14,12 +21,21 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.SignInButton;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.ivione93.myworkout.MainActivity;
 import com.ivione93.myworkout.R;
 import com.ivione93.myworkout.ui.profile.NewAthleteActivity;
 
 import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private SharedPreferences prefs;
+
+    EditText emailLogin, passwordLogin;
+    Button btnLogin, btnRegister;
+    ConstraintLayout loginLayout;
 
     private static final int RC_SIGN_IN = 111;
     private GoogleSignInClient mGoogleSignInClient;
@@ -32,6 +48,9 @@ public class LoginActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
         getIntent().removeExtra("license");
+        
+        initReferences();
+        checkSession();
 
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
@@ -56,6 +75,58 @@ public class LoginActivity extends AppCompatActivity {
             intent.setData(uri);
             startActivity(intent);
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        loginLayout.setVisibility(View.VISIBLE);
+    }
+
+    private void initReferences() {
+        emailLogin = findViewById(R.id.emailLogin);
+        passwordLogin = findViewById(R.id.passwordLogin);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+        loginLayout = findViewById(R.id.loginLayout);
+
+        /*btnRegister.setOnClickListener(v -> {
+            if (!emailLogin.getText().toString().isEmpty() && !passwordLogin.getText().toString().isEmpty()) {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(emailLogin.getText().toString(),
+                        passwordLogin.getText().toString()).addOnCompleteListener(it -> {
+                            if (it.isSuccessful()) {
+                                updateUI();
+                            } else {
+                                showAlert();
+                            }
+                });
+            }
+        });
+
+        btnLogin.setOnClickListener(v -> {
+            if (!emailLogin.getText().toString().isEmpty() && !passwordLogin.getText().toString().isEmpty()) {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(emailLogin.getText().toString(),
+                        passwordLogin.getText().toString()).addOnCompleteListener(it -> {
+                    if (it.isSuccessful()) {
+                        updateUI();
+                    } else {
+                        showAlert();
+                    }
+                });
+            }
+        });*/
+    }
+
+    private void checkSession() {
+        prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE);
+        String license = prefs.getString("license", null);
+
+        if (license != null) {
+            loginLayout.setVisibility(View.INVISIBLE);
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra("license", license);
+            startActivity(intent);
+        }
     }
 
     private void signIn() {
@@ -88,5 +159,12 @@ public class LoginActivity extends AppCompatActivity {
         Intent mainIntent = new Intent(this, NewAthleteActivity.class);
         mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(mainIntent);
+    }
+
+    private void showAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Error");
+        builder.setMessage("Se ha producido un error en la autenticación");
+        builder.create().show();
     }
 }
--- app/src/main/java/com/ivione93/myworkout/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/ivione93/myworkout/ui/profile/ProfileFragment.java
@@ -1,6 +1,8 @@
 package com.ivione93.myworkout.ui.profile;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -22,6 +24,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
 import com.ivione93.myworkout.R;
 import com.ivione93.myworkout.Utils;
 import com.ivione93.myworkout.db.AppDatabase;
@@ -39,6 +42,7 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class ProfileFragment extends Fragment {
 
     private ProfileViewModel profileViewModel;
+    private SharedPreferences.Editor prefs;
 
     CircleImageView photoProfile;
     TextView nameProfile, emailProfile, birthProfile, licenseProfile;
@@ -101,6 +105,11 @@ public class ProfileFragment extends Fragment {
             emailProfile.setText(account.getEmail());
             licenseProfile.setText(athlete.license);
             birthProfile.setText(Utils.toString(athlete.birthday));
+
+            // Guardado de datos inicio de sesion
+            prefs = getActivity().getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit();
+            prefs.putString("license", license);
+            prefs.apply();
         }
     }
 
@@ -131,6 +140,11 @@ public class ProfileFragment extends Fragment {
             getContext().startActivity(newCompetition);
         }
         if (item.getItemId() == R.id.menu_log_out) {
+            // Borrado datos inicio de sesion
+            prefs = getActivity().getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit();
+            prefs.clear();
+            prefs.apply();
+
             signOut();
             return true;
         }
@@ -205,6 +219,7 @@ public class ProfileFragment extends Fragment {
     }
 
     private void signOut() {
+        //FirebaseAuth.getInstance().signOut();
         mGoogleSignInClient.signOut()
                 .addOnCompleteListener(getActivity(), task -> {
                     Intent mainIntent = new Intent(getActivity().getApplicationContext(), LoginActivity.class);
